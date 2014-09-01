946b8fdf9aaff1c4c9f5e471c73c91130ed3a8f9
James Newman <jamesmnewman1@gmail.com>
2014-09-01 08:03:43 -0700

    fuck

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, "79e4fd1d574549478d00f857af9d4bc6", "c4989f31895d4ffda01f459a46a652e5", scope: 'playlist-modify-public playlist-modify-private playlist-read-private user-library-modify user-library-read user-read-email'
 end
